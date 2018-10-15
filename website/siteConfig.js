167a0b103931f9fed341b25dc42dfdf5a42f2066
Razvan Dinicut <razvan.dinicut@gmail.com>
2018-10-15 21:00:03 +0200

    GitHub pages deploy (#3)
    * fix travis file
    
    * publish only on master

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Centrifuge Developers', // Title for your website.
   tagline: '',
-  url: 'https://centrifuge.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://centrifuge.github.io', // Your website URL
+  baseUrl: '/developer.centrifuge.io/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
