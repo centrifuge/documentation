44de0e67676069d1f14b77cd0e262dc28e97793c
razvan <razvan@centrifuge.io>
2018-10-17 19:54:46 +0200

    fix build

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
-    //projectName: 'developer.centrifuge.io',
+    projectName: 'centrifuge',
     organizationName: 'centrifuge',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
