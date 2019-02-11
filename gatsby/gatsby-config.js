395bd68a6878f76f282f4db2326c7976403d3286
Jeff Reiner <theundeadghost@gmail.com>
2019-02-11 13:27:44 +0100

    :heavy_plus_sign: adding gatsby-plugin-sitemap

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
