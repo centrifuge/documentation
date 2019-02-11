f06a7f0fecb7f104a0a0c4ddf08fd3158382710f
Jeff Reiner <theundeadghost@gmail.com>
2019-02-11 13:17:59 +0100

    :sparkles: updating seo and site metadata

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Centrifuge Developer`,
+    siteUrl: process.env.URL || "http://localhost:8000",
+    description: `A Decentralized Operating System For The Financial Supply Chain`,
+    author: `@centrifuge`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,17 +18,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `centrifuge-developers`,
+        short_name: `developers`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#2762ff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/centrifuge-logo.png`
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ]
 };
