f829b6e623dc6decc79124b512757f56fdd221b0
Jeff Reiner <theundeadghost@gmail.com>
2019-02-12 15:55:22 +0100

    :sparkles: adding links and structure to the sidebar

--- gatsby/src/components/sidebar.js
+++ gatsby/src/components/sidebar.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { graphql, StaticQuery } from "gatsby";
+import { graphql, StaticQuery, Link } from "gatsby";
 
 const Sidebar = () => (
   <StaticQuery
     query={graphql`
       query {
-        allMdx {
-          edges {
-            node {
-              fields {
-                slug
-                title
+        allMdx(filter: { fields: { title: { ne: "404" } } }) {
+          group(field: fields___category) {
+            fieldValue
+            edges {
+              node {
+                fields {
+                  title
+                  slug
+                }
               }
             }
           }
@@ -18,12 +21,20 @@ const Sidebar = () => (
       }
     `}
     render={({ allMdx }) => (
-      <>
-        <p>Sidebar</p>
-        <pre>
-          <code>{JSON.stringify(allMdx.edges, null, 2)}</code>
-        </pre>
-      </>
+      <ul>
+        {allMdx.group.map((category, index) => (
+          <li key={index}>
+            <p>{category.fieldValue}</p>
+            <ul>
+              {category.edges.map((doc, index) => (
+                <li key={index}>
+                  <Link to={doc.node.fields.slug}>{doc.node.fields.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     )}
   />
 );
