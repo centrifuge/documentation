ea57668238f29c236a0385f3bea1f29f81be54bc
Jeff Reiner <theundeadghost@gmail.com>
2019-02-12 17:38:00 +0100

    :bug: fix bug with double forward slash

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -18,7 +18,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     createNodeField({
       name: `slug`,
       node,
-      value: `/${value}`
+      value
     });
 
     createNodeField({
