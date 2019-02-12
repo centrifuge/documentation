ea0e45e932891ecb5c2174928a90ac79f7227baa
Jeff Reiner <theundeadghost@gmail.com>
2019-02-12 18:06:29 +0100

    :bug: fixing bug with toc when no items in toc

--- gatsby/src/components/ToC/index.js
+++ gatsby/src/components/ToC/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const ToC = ({ content: { items } }) => (
-  <ul>
-    {items.map((level0, index) => (
-      <li key={index}>
-        <a href={level0.url}>{level0.title}</a>
-      </li>
-    ))}
-  </ul>
-);
+const ToC = ({ content }) => {
+  if (content.items)
+    return (
+      <ul>
+        {content.items.map((level0, index) => (
+          <li key={index}>
+            <a href={level0.url}>{level0.title}</a>
+          </li>
+        ))}
+      </ul>
+    );
+
+  return null;
+};
 
 export default ToC;
