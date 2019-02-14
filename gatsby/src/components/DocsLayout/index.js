9acb733e663f3a31334282668098aa83cea7be90
Jeff Reiner <theundeadghost@gmail.com>
2019-02-14 11:36:31 +0100

    Adding grommet & code highlighter components (#54)
    * :heavy_plus_sign: adding grommet grommet-icons styled-components styled-icons
    
    * Revert ":heavy_plus_sign: adding grommet grommet-icons styled-components styled-icons"
    
    This reverts commit 55e09094b3dd10f0d61bf3758d79454849131749.
    
    * :heavy_plus_sign: adding grommet and its deps properly
    
    * :sparkles: stubbing in grommet theme wrapper for now
    
    * :sparkles: adding grommet mdx mapping
    
    * :sparkles: extract component map to its own var
    
    * :sparkles: adding Code Highlighter
    
    * :heavy_minus_sign: removing grommet-icons

--- gatsby/src/components/DocsLayout/index.js
+++ gatsby/src/components/DocsLayout/index.js
@@ -2,15 +2,35 @@ import React from "react";
 import { graphql } from "gatsby";
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
 import { MDXProvider } from "@mdx-js/tag";
+import * as Grommet from "grommet";
 
 import Layout from "../Layout";
 import ToC from "../ToC";
+import CodeHighlighter from "../CodeHighlighter";
+
+const mdxGrommetMap = {
+  p: Grommet.Paragraph,
+  h1: props => <Grommet.Heading {...props} level={1} />,
+  h2: props => <Grommet.Heading {...props} level={2} />,
+  h3: props => <Grommet.Heading {...props} level={3} />,
+  h4: props => <Grommet.Heading {...props} level={4} />,
+  h5: props => <Grommet.Heading {...props} level={5} />,
+  h6: props => <Grommet.Heading {...props} level={6} />,
+  a: Grommet.Anchor,
+  img: Grommet.Image,
+  code: props => (
+    <CodeHighlighter
+      code={props.children.trim()}
+      language={String(props.className).split("language-")[1]}
+    />
+  )
+};
 
 const DocsLayout = ({ data: { mdx } }) => (
   <Layout>
     <h1>{mdx.frontmatter.title}</h1>
 
-    <MDXProvider components={{}}>
+    <MDXProvider components={mdxGrommetMap}>
       <MDXRenderer>{mdx.code.body}</MDXRenderer>
     </MDXProvider>
 
