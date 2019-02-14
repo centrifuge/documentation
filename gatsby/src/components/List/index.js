a3dfd50255d6ba70090a8eb409b6a9f3bcc11727
Jeff Reiner <theundeadghost@gmail.com>
2019-02-14 12:09:48 +0100

    :sparkles: adding a new List component

---
+++ gatsby/src/components/List/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Item = styled.li`
+  list-style: none;
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding-left: 0;
+`;
