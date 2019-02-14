c710e338b7e0d5318672bbf00cdadc31856b62d9
Jeff Reiner <theundeadghost@gmail.com>
2019-02-14 16:26:11 +0100

    :recycle: moving MDX section to sep component

--- gatsby/src/components/DocsLayout/index.js
+++ gatsby/src/components/DocsLayout/index.js
@@ -1,49 +1,27 @@
 import React from "react";
 import { graphql } from "gatsby";
-import MDXRenderer from "gatsby-mdx/mdx-renderer";
-import { MDXProvider } from "@mdx-js/tag";
-import * as Grommet from "grommet";
+import { Heading, Box } from "grommet";
 
 import Layout from "../Layout";
 import TableOfContents from "../TableOfContents";
-import CodeHighlighter from "../CodeHighlighter";
 import Sidebar from "../Sidebar";
-
-const mdxGrommetMap = {
-  p: Grommet.Paragraph,
-  h1: props => <Grommet.Heading {...props} level={1} />,
-  h2: props => <Grommet.Heading {...props} level={2} />,
-  h3: props => <Grommet.Heading {...props} level={3} />,
-  h4: props => <Grommet.Heading {...props} level={4} />,
-  h5: props => <Grommet.Heading {...props} level={5} />,
-  h6: props => <Grommet.Heading {...props} level={6} />,
-  a: Grommet.Anchor,
-  img: Grommet.Image,
-  code: props => (
-    <CodeHighlighter
-      code={props.children.trim()}
-      language={String(props.className).split("language-")[1]}
-    />
-  )
-};
+import DocsContent from "../DocsContent";
 
 const DocsLayout = ({ data: { mdx } }) => (
   <Layout>
-    <Grommet.Box gridArea="sidebar" as="aside">
+    <Box gridArea="sidebar" as="aside">
       <Sidebar />
-    </Grommet.Box>
+    </Box>
 
-    <Grommet.Box gridArea="main" as="main">
-      <Grommet.Heading level={1}>{mdx.frontmatter.title}</Grommet.Heading>
+    <Box gridArea="main" as="main">
+      <Heading level={1}>{mdx.frontmatter.title}</Heading>
 
-      <MDXProvider components={mdxGrommetMap}>
-        <MDXRenderer>{mdx.code.body}</MDXRenderer>
-      </MDXProvider>
-    </Grommet.Box>
+      <DocsContent mdx={mdx} />
+    </Box>
 
-    <Grommet.Box gridArea="toc" as="aside">
+    <Box gridArea="toc" as="aside">
       <TableOfContents content={mdx.tableOfContents} />
-    </Grommet.Box>
+    </Box>
   </Layout>
 );
 
