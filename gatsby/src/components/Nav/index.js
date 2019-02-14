2970680b4d5a417aab940c89d4d50b774690d026
Jeff Reiner <theundeadghost@gmail.com>
2019-02-14 17:45:18 +0100

    rough stub in of the nav (#57)

--- gatsby/src/components/Nav/index.js
+++ gatsby/src/components/Nav/index.js
@@ -1,48 +1,49 @@
 import React from "react";
+import { Anchor, Box } from "grommet";
+
+import { Item } from "../List";
+
+const ExternalAnchor = ({ href, children }) => (
+  <Anchor target="_blank" rel="noopener noreferrer" href={href}>
+    {children}
+  </Anchor>
+);
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>Logo</li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://centrifuge-os-node-api.api-docs.io/0.0.3/"
-        >
+  <Box as="nav">
+    <Box
+      as="ul"
+      gap="large"
+      direction="row"
+      style={{ listStyle: "none" }}
+      pad={{ vertical: "medium", left: "none" }}
+    >
+      <Box flex="grow" as="li">
+        Logo
+      </Box>
+      <Item>
+        <ExternalAnchor href="https://centrifuge-os-node-api.api-docs.io/0.0.3/">
           Node API
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/centrifuge"
-        >
+        </ExternalAnchor>
+      </Item>
+      <Item>
+        <ExternalAnchor href="https://github.com/centrifuge">
           GitHub
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://centrifuge.io/centrifuge_os_white_paper.pdf"
-        >
+        </ExternalAnchor>
+      </Item>
+      <Item>
+        <ExternalAnchor href="https://centrifuge.io/centrifuge_os_white_paper.pdf">
           Whitepaper
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://join.slack.com/t/centrifuge-io/shared_invite/enQtNDk1MzkwODM4OTgxLWRlNTU4NDQzOWIwYWEzNGRhN2UzMzQwNThjZjI0ZmIxMTU4NmQwMjc2ZDBkOTEyNWJhMjE4MzA2NTE5MWU1NWE"
-        >
+        </ExternalAnchor>
+      </Item>
+      <Item>
+        <ExternalAnchor href="https://join.slack.com/t/centrifuge-io/shared_invite/enQtNDk1MzkwODM4OTgxLWRlNTU4NDQzOWIwYWEzNGRhN2UzMzQwNThjZjI0ZmIxMTU4NmQwMjc2ZDBkOTEyNWJhMjE4MzA2NTE5MWU1NWE">
           Slack
-        </a>
-      </li>
-      <li>🔍</li>
-    </ul>
-  </nav>
+        </ExternalAnchor>
+      </Item>
+      <Item>🔍</Item>
+    </Box>
+  </Box>
 );
 
 export default Nav;
