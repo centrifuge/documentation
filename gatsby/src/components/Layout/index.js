abf0a515a7e2f565d96f8508e956b7ba4e6f749f
Jeff Reiner <theundeadghost@gmail.com>
2019-02-26 17:59:36 +0100

    :sparkles: adding bottom footer

--- gatsby/src/components/Layout/index.js
+++ gatsby/src/components/Layout/index.js
@@ -12,18 +12,20 @@ const Layout = ({ children }) => (
       margin={{ horizontal: "auto" }}
       fill
       columns={Array(12).fill("1fr")}
-      rows={["auto", "auto"]}
+      rows={Array(3).fill("auto")}
       areas={[
         { name: "header", start: [0, 0], end: [12, 0] },
         { name: "sidebar", start: [0, 1], end: [3, 1] },
         { name: "main", start: [3, 1], end: [9, 1] },
-        { name: "toc", start: [9, 1], end: [12, 1] }
+        { name: "toc", start: [9, 1], end: [12, 1] },
+        { name: "footer", start: [0, 2], end: [12, 2] }
       ]}
     >
       <Box gridArea="header" as="header">
         <Nav />
       </Box>
       {children}
+      <Box gridArea="footer" as="footer" pad="large" />
     </Grid>
   </AxisTheme>
 );
