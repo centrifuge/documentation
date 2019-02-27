475d1b58ccfdb3b6c47187b08a2dcb34e8805f26
Jeff Reiner <theundeadghost@gmail.com>
2019-02-27 14:01:11 +0100

    :sparkles: adding better default list component

--- gatsby/src/components/DocsContent/index.js
+++ gatsby/src/components/DocsContent/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
 import { MDXProvider } from "@mdx-js/tag";
+import styled from "styled-components";
 import * as Grommet from "grommet";
 
 import CodeHighlighter from "../CodeHighlighter";
 
+const ListBase = styled(Grommet.Text)`
+  padding-left: 1rem;
+`;
+
 const mdxGrommetMap = {
   p: Grommet.Paragraph,
-  h1: props => <Grommet.Heading {...props} level={1} lined />,
-  h2: props => <Grommet.Heading {...props} level={2} lined />,
+  h1: props => <Grommet.Heading {...props} level={1} />,
+  h2: props => <Grommet.Heading {...props} level={2} />,
   h3: props => <Grommet.Heading {...props} level={3} />,
   h4: props => <Grommet.Heading {...props} level={4} />,
   h5: props => <Grommet.Heading {...props} level={5} />,
   h6: props => <Grommet.Heading {...props} level={6} />,
   li: props => <Grommet.Text {...props} as="li" />,
+  ul: props => <ListBase {...props} as="ul" />,
+  ol: props => <ListBase {...props} as="ol" />,
   a: Grommet.Anchor,
   img: Grommet.Image,
   code: props => (
