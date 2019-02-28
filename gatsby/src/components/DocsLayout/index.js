70f720e1370a03671b934e20d0fa0ce35bb2c73b
Jeff Reiner <theundeadghost@gmail.com>
2019-02-28 15:14:17 +0100

    :sparkles: adding edit page component

--- gatsby/src/components/DocsLayout/index.js
+++ gatsby/src/components/DocsLayout/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { Heading, Box, ResponsiveContext } from "grommet";
+import { Heading, Box, ResponsiveContext, Anchor } from "grommet";
 
 import Layout from "../Layout";
 import TableOfContents from "../TableOfContents";
 import Sidebar from "../Sidebar";
 import DocsContent from "../DocsContent";
+
+const EditPage = ({ path }) => {
+  const GITHUB_BASE =
+    "https://github.com/centrifuge/developer.centrifuge.io/tree/develop/";
+  const githubLink = `${GITHUB_BASE}${
+    path.match(/.*(developer.centrifuge.io)(.*)/)[2]
+  }`;
+
+  return (
+    <Box margin={{ top: "large" }}>
+      <Anchor href={githubLink}>Edit this page on GitHub</Anchor>
+    </Box>
+  );
+};
 
 const DocsLayout = ({ data: { mdx } }) => (
   <Layout>
@@ -22,6 +36,8 @@ const DocsLayout = ({ data: { mdx } }) => (
             </Heading>
 
             <DocsContent mdx={mdx} />
+
+            <EditPage path={mdx.fileAbsolutePath} />
           </Box>
 
           {size === "large" && (
@@ -39,6 +55,7 @@ export const query = graphql`
   query DocsQuery($id: String) {
     mdx(id: { eq: $id }) {
       id
+      fileAbsolutePath
       frontmatter {
         title
       }
