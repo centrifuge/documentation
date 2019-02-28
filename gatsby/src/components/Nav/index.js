6e7bcc69f5acc6e7bba549589f8ff5f4767f728d
Jeff Reiner <theundeadghost@gmail.com>
2019-02-28 15:48:03 +0100

    :bug: remove duplicate nav

--- gatsby/src/components/Nav/index.js
+++ gatsby/src/components/Nav/index.js
@@ -38,33 +38,6 @@ const Nav = () => (
           </Link>
         </div>
       </Box>
-      <Box as="li">
-        <Box as="ul" direction="row" align="center" gap="large">
-          <Item>
-            <ExternalAnchor href="https://centrifuge-os-node-api.api-docs.io/0.0.3/">
-              Node API
-            </ExternalAnchor>
-          </Item>
-          <Item>
-            <ExternalAnchor href="https://github.com/centrifuge">
-              GitHub
-            </ExternalAnchor>
-          </Item>
-          <Item>
-            <ExternalAnchor href="https://centrifuge.io/centrifuge_os_white_paper.pdf">
-              Whitepaper
-            </ExternalAnchor>
-          </Item>
-          <Item>
-            <ExternalAnchor href="https://centrifuge.io/slack/">
-              Slack
-            </ExternalAnchor>
-          </Item>
-          <ListItem>
-            <Search />
-          </ListItem>
-        </Box>
-      </Box>
       <ResponsiveContext.Consumer>
         {size =>
           size !== "small" && (
