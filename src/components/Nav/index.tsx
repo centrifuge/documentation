37ca9d634ed65d0cff85c1bcc88802c2d2fd9cde
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-03-07 06:48:04 -0500

    Fix logo alignment

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-import { AxisTheme } from "@centrifuge/axis-theme";
-import { Box, Image } from "grommet";
-import { NavBar } from "@centrifuge/axis-nav-bar";
-import { MenuItem } from "@centrifuge/axis-nav-bar";
-import { Menu as MenuIcon, User as UserIcon, Close as CloseIcon } from "grommet-icons";
+import {Box, Image, Grid} from "grommet";
+import {NavBar} from "@centrifuge/axis-nav-bar";
+import {MenuItem} from "@centrifuge/axis-nav-bar";
+import {Menu as MenuIcon, User as UserIcon, Close as CloseIcon} from "grommet-icons";
 import Search from "../Search";
 import styled from "styled-components";
-import { navigate } from "gatsby";
-const wordmark = require("../../images/centrifuge-developer-wordmark.svg") as string;
+import {navigate} from "gatsby";
+const wordmark = require("../../images/centrifuge-developer-wordmark.svg")as string;
 
 
 const Logo = styled(Image)`
@@ -17,63 +16,100 @@ const Logo = styled(Image)`
   margin: 16px 0;
 `;
 
-  const Nav = (props) => {
-      const [selectedRoute, setSelectedRoute] = useState("/");
-      const menuItems: MenuItem[] = [
+const Nav = (props) => {
+    const [selectedRoute, setSelectedRoute] = useState("/");
+    const menuItems: MenuItem[] = [
         {
-          label: "Centrifuge P2P Node",
-          route: "/cent-node/"
+            label: "Centrifuge P2P Node",
+            route: "/cent-node/"
         },
         {
-          label: "Tinlake",
-          route: "/tinlake/"
+            label: "Tinlake",
+            route: "/tinlake/"
         },
         {
-          label: "NFTs",
-          route: "/nfts/overview/introduction/"
+            label: "NFTs",
+            route: "/nfts/overview/introduction/"
         },
         {
-          label: 'Centrifuge Chain',
-          route: "/chain/"
-        },
-        {
-          label: 'Github',
-          route: "https://github.com/centrifuge"
-        },
-        {
-          label: 'Slack',
-          route: "https://centrifuge.io/slack/"
+            label: 'Centrifuge Chain',
+            route: "/chain/"
+        }, {
+            label: 'Github',
+            route: "https://github.com/centrifuge"
+        }, {
+            label: 'Slack',
+            route: "https://centrifuge.io/slack/"
         }
-      ];
-      const onRouteClick = (route) => {
+    ];
+    const onRouteClick = (route) => {
         setSelectedRoute(route);
-        if (route.startsWith('/')){
-          navigate(route);
+        if (route.startsWith('/')) {
+            navigate(route);
+        } else {
+            window.open(route);
         }
-        else {
-          window.open(route);
+    };
+    const theme = {
+        navBar: {
+            icons: {
+                menu: MenuIcon,
+                close: CloseIcon,
+                user: UserIcon
+            }
         }
-      };
-      const theme = { navBar: { icons: {
-        menu: MenuIcon,
-        close: CloseIcon,
-        user: UserIcon,
-      }}};
-   
-      return (
-        <Box>
-            <NavBar
-              menuItems={menuItems}
-              theme={theme}
-              selectedRoute={selectedRoute}
-              onRouteClick={(item: MenuItem) => {onRouteClick(item.route);}}
-              logo={<Logo src={wordmark} onClick={() => {onRouteClick('/')}} />}
-              width={"100%"}
-            />
-            <Search />
-        </Box>
+    };
 
-      );
-  };
+    return (
+        <Grid fill
+            areas={
+                [
+                    {
+                        name: "logo",
+                        start: [
+                            0, 0
+                        ],
+                        end: [0, 0]
+                    }, {
+                        name: "navBar",
+                        start: [
+                            1, 0
+                        ],
+                        end: [1, 0]
+                    }
+                ]
+            }
+            columns={
+                ["small", "flex"]
+            }
+            rows={
+                ["flex"]}
+                margin={{left:"large"}}>
+            <Logo src={wordmark}
+                alignSelf="start"
+                gridArea="logo"
+                onClick={
+                    () => {
+                        onRouteClick('/')
+                    }
+                
+                }/>
+            <Box direction="row" gridArea="navBar">
+
+                <NavBar align="end"
+                    menuItems={menuItems}
+                    theme={theme}
+                    width={"100%"}
+                    selectedRoute={selectedRoute}
+                    onRouteClick={
+                        (item : MenuItem) => {
+                            onRouteClick(item.route);
+                        }
+                }>
+                    <Search/></NavBar>
+            </Box>
+        </Grid>
+    );
+};
 
 export default Nav;
