a627f383cabcf704750e450fa203e0d39091eadd
sophian <littlejohn.sophia@gmail.com>
2024-04-12 18:06:54 -0400

    Escape moe characters and convert tsx to js

--- src/pages/centrifuge-os-node-api.js
+++ src/pages/centrifuge-os-node-api.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { ResponsiveContext } from "grommet";
-import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 import spec from "../assets/swagger.json";
@@ -8,12 +7,20 @@ import Layout from "../components/Layout";
 import { theme } from "../theme";
 import { AxisTheme } from "@centrifuge/axis-theme";
 
+const SwaggerUI = React.lazy(() =>
+  typeof window !== "undefined"
+    ? import("swagger-ui-react")
+    : Promise.resolve({ default: () => null })
+);
+
 export default () => (
   <AxisTheme theme={theme}>
     <ResponsiveContext.Consumer>
       {(size) => (
         <Layout size={size}>
-          <SwaggerUI spec={spec} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <SwaggerUI spec={spec} />
+          </Suspense>
         </Layout>
       )}
     </ResponsiveContext.Consumer>
